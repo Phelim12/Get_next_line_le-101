838682f6454acc6382cc61411c5e45ac9ce7c798
Clement Creusot <clcreuso@z1r10p3.le-101.fr>
2018-04-07 23:29:52 +0200

    Update: Strdup->Save -- Launch V1.0.0

--- Rendu/get_next_line.c
+++ Rendu/get_next_line.c
@@ -19,7 +19,7 @@ static int	ft_read_file(char **str, int fd)
 	char	*tmp;
 	int		ret;
 
-	if (!(sample = (char *)malloc(sizeof(*sample) * (BUFF_SIZE + 1))))
+	if (!(sample = ft_strnew(BUFF_SIZE + 1)))
 		return (0);
 	ret = read(fd, sample, BUFF_SIZE);
 	if (ret > 0)
@@ -42,7 +42,7 @@ int			get_next_line(int const fd, char **line)
 	if ((fd < 0 || line == NULL || read(fd, str, 0) < 0) ||
 	(!str && !(str = ft_strnew(0))))
 		return (-1);
-	while (!(save = ft_strchr(str, '\n')))
+	while (!(ft_strchr(str, '\n')))
 	{
 		ret = ft_read_file(&str, fd);
 		if ((ret == 0) && !(ft_strlen(str)))
@@ -52,9 +52,12 @@ int			get_next_line(int const fd, char **line)
 		if (ret < 0)
 			return (-1);
 	}
+	if (ft_strchr(str, '\n'))
+		save = ft_strdup(ft_strchr(str, '\n'));
 	*line = ft_strsub(str, 0, ft_strlen(str) - ft_strlen(save));
 	ft_strdel(&str);
-	if ((save + 1)[0] != 0)
+	if (save && (save + 1)[0] != 0)
 		str = ft_strdup(save + 1);
+	ft_strdel(&save);
 	return (1);
 }
